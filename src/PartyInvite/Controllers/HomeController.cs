e9d11c889f09e299924a3b5c888b2cf1fac99117
Daniel Spencer <DSpencer@assessor.lacounty.gov>
2019-01-22 08:52:40 -0800

    test commit 2

--- src/PartyInvite/Controllers/HomeController.cs
+++ src/PartyInvite/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace PartyInvite.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "My Contact contact page.";
 
             return View();
         }
